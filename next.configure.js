d0538ade166d0b1d6ad97b6558c51ee58553ceec
Sergiogz29 <sergiogarzam33@gmail.com>
2025-06-02 20:43:25 +0200

    nuevo commit

---
+++ next.configure.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Esto le dice a Next que, cuando hagas `next build`, genere la versión estática en la carpeta `out/`
+  output: "export"
+};
+
+module.exports = nextConfig;
